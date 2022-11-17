1abac9ba894b48d0e422e4638926e27eaee82e0c
Colin Fahrion <colin.fahrion@gmail.com>
2022-11-16 22:37:04 -0800

    feat: home page top logo svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,60 +1,58 @@
-const scale = {
-    '3xs': '0.146rem',
-    '2xs': '0.236rem',
-    xs: '0.382rem',
-    sm: '0.618rem',
-    base: '1rem',
-    md: '1.618rem',
-    lg: '2.618rem',
-    xl: '4.236rem',
-    '2xl': '6.854rem'
-}
+const defaultTheme = require("tailwindcss/defaultTheme");
 
-const colors = require('tailwindcss/colors')
+const scale = {
+	"3xs": "0.146rem",
+	"2xs": "0.236rem",
+	xs: "0.382rem",
+	sm: "0.618rem",
+	base: "1rem",
+	md: "1.618rem",
+	lg: "2.618rem",
+	xl: "4.236rem",
+	"2xl": "6.854rem",
+};
+
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: [
-    "./public/**/*.html",
-    "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
-  ],
-  important: true,
-  theme: {
-    borderColor: (theme) => ({
-      ...theme("colors"),
-      default: theme("colors.black", "currentColor"),
-    }),
-    inset: (theme, { negative }) => ({
-      ...theme("spacing"),
-      ...negative(theme("spacing")),
-      0: "0",
-      auto: "auto",
-    }),
-    extend: {
-      borderRadius: {
-        xl: ".75rem",
-      },
-      colors: {
-        body: colors.stone[800]
-      },
-      spacing: scale,
-      fontFamily: {
-        display: ['superclarendon', 'serif']
-      },
-      fontSize: {
-        "3xs": "0.5rem",
-        "2xs": "0.625rem",
-        "7xl": "5rem",
-        "8xl": "6rem",
-        "9xl": "7rem",
-      },
-      typography: {
-        css: {
-            color: colors.stone[800]
-        }
-      }
-    },
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+	content: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
+	important: true,
+	theme: {
+		borderColor: (theme) => ({
+			...theme("colors"),
+			default: theme("colors.black", "currentColor"),
+		}),
+		inset: (theme, { negative }) => ({
+			...theme("spacing"),
+			...negative(theme("spacing")),
+			0: "0",
+			auto: "auto",
+		}),
+		extend: {
+			borderRadius: {
+				xl: ".75rem",
+			},
+			colors: {
+				body: colors.stone[800],
+			},
+			spacing: scale,
+			fontFamily: {
+				display: ["superclarendon", ...defaultTheme.fontFamily.serif],
+				sans: ["trade-gothic-next", ...defaultTheme.fontFamily.sans],
+			},
+			fontSize: {
+				"3xs": "0.5rem",
+				"2xs": "0.625rem",
+				"7xl": "5rem",
+				"8xl": "6rem",
+				"9xl": "7rem",
+			},
+			typography: {
+				css: {
+					color: colors.stone[800],
+				},
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
 };
