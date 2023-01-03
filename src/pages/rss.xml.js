f49cf18574441e3a839a3c02a7bf1b4c35c5f427
Colin Fahrion <colin.fahrion@gmail.com>
2023-01-03 06:51:16 -0800

    feat: add webring

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,26 @@
 import rss from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
 
 const postImportResult = import.meta.globEager("./posts/*.md");
 const posts = Object.values(postImportResult);
 
-export const get = () =>
-	rss({
+export const get = async () => {
+	const feed = await rss({
 		title: "Mirth Peddlers",
 		description: "Games, LARPs, and mirthful shenanigans!",
 		site: import.meta.env.SITE,
-		items: import.meta.glob("./posts/**/*.md"),
+		items: posts.map((post) => {
+			return {
+				link: post.url,
+				title: post.frontmatter.title,
+				pubDate: post.frontmatter.pubDate,
+				description: post.frontmatter.description,
+				content: "hi hi hi",
+			};
+		}),
 	});
+
+	console.log(feed);
+
+	return feed;
+};
