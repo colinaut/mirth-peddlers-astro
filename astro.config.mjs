69f8b84a95b9f18bfe5383c9e97741cc83169900
Colin Fahrion <colin.fahrion@gmail.com>
2023-05-10 21:03:39 -0700

    feat: add astro image and sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [sitemap()],
+	integrations: [
+		sitemap(),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+	],
 	site: "https://www.mirthpeddlers.com/",
 });
